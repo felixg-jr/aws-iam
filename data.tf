f7fce1de3adb7349f535ccf6d7cec542f348a421
Jr G <jg@example.com>
2024-04-06 20:17:29 -0500

    blah!

---
+++ data.tf
@@ -0,0 +1,14 @@
+# The SNS policy
+data "aws_iam_policy_document" "sns_topic_policy" {
+  statement {
+    effect  = "Allow"
+    actions = ["SNS:Publish"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["events.amazonaws.com"]
+    }
+
+    resources = [aws_sns_topic.aws_ps_create.arn]
+  }
+}
